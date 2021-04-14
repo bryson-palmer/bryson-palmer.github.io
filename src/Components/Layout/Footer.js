f86793791b51cd158d275f392e1d20de60958f45
Bryson Palmer <brysonpalmer@gmail.com>
2021-04-14 01:19:26 -0700

    Added tool tips to the footer icons.

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -5,6 +5,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -26,6 +27,7 @@ const useStyles = makeStyles((theme) => ({
         color: '#fff',
         textAlign: "center",
         paddingBottom: '2rem',
+        fontSize: '0.8rem',
     }
 }));
 
@@ -35,7 +37,7 @@ function Copyright() {
       <Typography className={classes.copyright} >
         {'© '}
         {new Date().getFullYear()}
-        {'Bryson Palmer'}
+        {' Bryson Palmer | Full-Stack | Developer'}
       </Typography>
     );
 }
@@ -46,21 +48,27 @@ function Footer() {
     return (
         <div>
             <div className={classes.root}>
-                <IconButton
-                    aria-label='GitHub.com'
-                    onClick={() => window.open('https://github.com/bryson-palmer', '_blank')}>
-                    <GitHubIcon className={classes.git} />
-                </IconButton>
-                <IconButton 
-                    aria-label='Linkedin.com' 
-                    onClick={() => window.open('https://www.linkedin.com/in/bryson-palmer-555b3862/', '_blank')}>
-                    <LinkedInIcon className={classes.linked} />
-                </IconButton>
-                <IconButton
-                    aria-label='My Resume'
-                    onClick={() => window.open('https://docs.google.com/document/d/19WL4DclHMyKQ1_jmdgUm_kBA3A-d0P4o2hpSkjHySFw/edit?usp=sharing', '_blank')}>
-                    <ListAltIcon className={classes.resume} />
-                </IconButton>
+                <Tooltip title='GitHub' arrow>
+                    <IconButton
+                        aria-label='GitHub.com'
+                        onClick={() => window.open('https://github.com/bryson-palmer', '_blank')}>
+                        <GitHubIcon className={classes.git} />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title='Linkedin' arrow>
+                    <IconButton 
+                        aria-label='Linkedin.com' 
+                        onClick={() => window.open('https://www.linkedin.com/in/bryson-palmer-555b3862/', '_blank')}>
+                        <LinkedInIcon className={classes.linked} />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title='My Resume' arrow>
+                    <IconButton
+                        aria-label='My Resume'
+                        onClick={() => window.open('https://docs.google.com/document/d/19WL4DclHMyKQ1_jmdgUm_kBA3A-d0P4o2hpSkjHySFw/edit?usp=sharing', '_blank')}>
+                        <ListAltIcon className={classes.resume} />
+                    </IconButton>
+                </Tooltip>
             </div>
             <Copyright />
         </div>
