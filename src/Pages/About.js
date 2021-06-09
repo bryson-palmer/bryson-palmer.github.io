a8d3433d0a60236979cda9249c8a10eb3cbf9e9b
Bryson Palmer <brysonpalmer@gmail.com>
2021-06-09 11:21:21 -0700

    Setting up the layout for the about me page.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Fade } from '@material-ui/core';
 import VizSensor from 'react-visibility-sensor';
@@ -7,7 +7,7 @@ import AboutMe from '../Components/AboutMe';
 import ContactMe from '../Components/Layout/ContactMe';
 import Footer from '../Components/Layout/Footer';
 // import Galaxy from './assets/galaxy.mp4';
-import { YoutubePlayer } from 'reactjs-media';
+// import { YoutubePlayer } from 'reactjs-media';
 import useWindowWidth from '../hook/useWindowWidth';
 
 const useStyles = makeStyles(() => ({
@@ -23,30 +23,31 @@ const useStyles = makeStyles(() => ({
     alignContent: 'center',
     background: '#1c1c1c',
     backgroundSize: 'cover',
-  },
+},
+
   // iframe: {
   //   minHeight: '90vh',
   //   minWidth: '100vw',
   //   marginTop: '10vh'
   // },
-  video: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    top: 0
-  },
+  // video: {
+  //   width: '100%',
+  //   height: '100%',
+  //   position: 'absolute',
+  //   top: 0
+  // },
   content: {
-    position: 'relative',
-    top: 0,
-    color: '#f1f1f1',
-    backgroundColor: '#1c1c1c',
-    width: '100vw',
-    height: '100%',
     display: 'flex',
-    flexDirection: 'column',
     justifyContent: 'center',
-    // alignItems: 'center',
-    padding: 0,
+    alignItems: 'center',
+    minHeight: '100vh',
+    minWidth: '100vw',
+    color: '#fff',
+    fontFamily: 'Righteous, cursive',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/milkyway.jpg'})`,
+    backgroundBlendMode: 'multiply',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
   },
 
 }));
@@ -67,6 +68,14 @@ const useStyles = makeStyles(() => ({
 // Your browser does not support the video tag.
 // </video>
 
+// <YoutubePlayer
+  // className={classes.video}
+  // src='https://youtu.be/iE7PBovWh-E'
+  // width={'100%'}
+  // height={1500}
+  // allowFullScreen
+// />
+
 export default function About() {
   const classes = useStyles();
   const windowY = useWindowWidth().height;
@@ -75,32 +84,35 @@ export default function About() {
   // State for fading in and out the header
   const [activeHeader, setActiveHeader] = useState(true);
 
+  // State for helping delay welcome message by delaying check
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(true);
+}, []);
+
   return (
-    <VizSensor
-      partialVisibility={true}
-      offset={{ top: windowX < windowY ? 500 : 250 }}
-      scrollDelay={10}
-      onChange={(isVisible) => {
-        setActiveHeader(isVisible);
-      }}
-    >
-      <Fade in={activeHeader} timeout={1500} >
-        <div className={classes.root}>
-          {/* <YoutubePlayer
-            className={classes.video}
-            src='https://youtu.be/iE7PBovWh-E'
-            width={'100%'}
-            height={1500}
-            allowFullScreen
-          /> */}
-          <div className={classes.content} >
-            <NavBar />
+    <div className={classes.root}>
+      <VizSensor
+        partialVisibility={true}
+        offset={{ top: windowX < windowY ? 500 : 250 }}
+        scrollDelay={10}
+        onChange={(isVisible) => {
+          setActiveHeader(isVisible);
+        }}
+      >
+        <Fade in={activeHeader} timeout={1500} >
+          <div 
+            className={classes.content} 
+            style={{backgroundColor: checked ? '#F6B2FF' : '#1c1c1c'}}
+            id='header'>
             <AboutMe />
-            <ContactMe />
-            <Footer />
           </div>
-        </div>
-      </Fade>
-    </VizSensor>
+        </Fade>
+      </VizSensor>
+      <NavBar />
+      <ContactMe />
+      <Footer />
+    </div>
   );
 }
