41af4c8a7817a666a84f42c005fb62814847149f
Bryson Palmer <brysonpalmer@gmail.com>
2024-02-20 15:48:58 -0800

    Updated vite.config.js plugin section with vite-plugin-pwa.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from 'vite-plugin-eslint'
+import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
@@ -12,6 +13,55 @@ export default defineConfig({
       cache: false,
       include: ['./src/**/*.js', './src/**/*.jsx'],
       exclude: [],
+    }),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: '/apple-splash-landscape-2048x1536.png',
+            sizes: '2048x1536',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/apple-splash-portrait-1536x2048.png',
+            sizes: '1536x2048',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/apple-touch-icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src:  '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      registerType: 'autoUpdate'
     })
   ],
   resolve: {
