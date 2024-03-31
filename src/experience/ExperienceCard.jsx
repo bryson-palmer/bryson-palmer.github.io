c6210d8da34ad9cfe40d9cf618287cd4d82b7d6b
Bryson Palmer <brysonpalmer@gmail.com>
2024-03-31 09:26:15 -0700

    Adding jsx and styles to incorporate a read more action for the experience section.

--- src/experience/ExperienceCard.jsx
+++ src/experience/ExperienceCard.jsx
@@ -1,3 +1,7 @@
+import { useCallback, useState } from 'react'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Button from '@mui/material/Button'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import Link from '@mui/material/Link'
 import PropTypes from 'prop-types'
 import './experience.css'
@@ -23,6 +27,10 @@ export const ExperienceCard = ({
   time = '',
   companyInfo = {},
 }) => {
+  const [showMore, setShowMore] = useState(false)
+  const isMobileLandscape = useMediaQuery('(max-width: 900px) and (orientation: landscape)')
+
+  const toggleShowMore = useCallback(() => setShowMore(!showMore), [showMore])
   const {
     companyName = '',
     companyLink = '',
@@ -33,45 +41,69 @@ export const ExperienceCard = ({
   } = companyInfo
 
   return (
-    <div className='experience-card'>
-      <div className='experience-header'>
-        <div className='experience-title'>
+    <div className="experience-card">
+      <div className="experience-header">
+        <div className="experience-title">
           <Link
-            className='experience-link'
+            className="experience-link"
             href={companyLink}
-            target='_blank'
-            rel='noreferrer'
+            target="_blank"
+            rel="noreferrer"
           >
             {companyName}
           </Link>
           {formerName ? (
             <Link
-              className='experience-link'
+              className="experience-link"
               href={formerLink}
-              target='_blank'
-              rel='noreferrer'
+              target="_blank"
+              rel="noreferrer"
             >
               {formerName}
-            </Link>) : null}
+            </Link>
+          ) : null}
           <Link
-            className='experience-link'
+            className="experience-link"
             href={appLink}
-            target='_blank'
-            rel='noreferrer'
+            target="_blank"
+            rel="noreferrer"
           >
             / {appName}
           </Link>
         </div>
-        <div className='experience-role'>
+        <div className="experience-role">
           <i>{position}</i>
-          <p className='numeral-text'>{time}</p>
+          <p className="numeral-text">{time}</p>
         </div>
       </div>
-      <div>
+      <div
+        className="experience-text"
+        style={
+          isMobileLandscape && !showMore
+            ? {
+                maxHeight: "134px",
+                overflowY: "hidden",
+              }
+            : {
+                maxHeight: "100%",
+                overflowY: "visible",
+              }
+        }
+      >
         {text.map((description, index) => (
-          <p key={index} className='experience-text'>{description}</p>
+          <p key={index}>{description}</p>
         ))}
       </div>
+      <Button
+        className='read-more-experience'
+        aria-label='read more'
+        variant='text'
+        onClick={toggleShowMore}
+        endIcon={<ExpandMoreIcon sx={{ transform: showMore ? 'rotate(180deg)' : 'rotate(0deg)' }} />}
+        href={showMore ? '#experience' : ''}
+      >
+        {showMore ? 'Read less' : 'Read more'}
+      </Button>
     </div>
   )
 }
