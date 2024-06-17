9afffcd6ff14b4f7538be43b4a47798f1140ecd7
Bryson Palmer <brysonpalmer@gmail.com>
2024-06-17 16:36:28 -0700

    Adding intersection observer and variables to project component.

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -1,21 +1,33 @@
-import { Section } from '@/ui'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import { PROJECTS_LIST } from './constants'
 import { ProjectCard } from './ProjectCard'
+import { Section } from '@/ui'
+import { createObserver } from '@/utils'
 import './projects.css'
+import { useMemo } from 'react'
 
 export const Projects = () => {
+  const isMobile = useMediaQuery('(max-width: 600px')
+  const isMobileLandscape = useMediaQuery('(max-width: 900px) and (orientation: landscape)')
+  const observerThreshold = useMemo(() => isMobile ? 0.3 : 0.5, [isMobile])
+  const observerMargin =  useMemo(() => isMobileLandscape ? '0px': '-100px 0px',[isMobileLandscape]) 
+
+  // Creates IntersectionObserver by id 
+  createObserver('#projects-observer', { rootMargin: observerMargin, threshold: observerThreshold })
+
   return (
     <Section id={'projects'} title={'Projects'}>
-      <div className='project-container'>
-        {PROJECTS_LIST.map((project, index) => (
-          <ProjectCard
-            key={index}
-            {...project}
-          />
-        ))}
+      <div id='projects-observer' >
+        <div className='project-container'>
+          {PROJECTS_LIST.map((project, index) => (
+            <ProjectCard
+              key={index}
+              {...project}
+            />
+          ))}
+        </div>
+        <strong className='small-display'>&lt;- Scroll for more projects -&gt;</strong>
       </div>
-      <strong className='small-display'>&lt;- Scroll -&gt;</strong>
-      <p className='small-display'>For more projects</p>
     </Section>
   )
 }
